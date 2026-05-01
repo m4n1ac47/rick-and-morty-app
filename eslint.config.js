495e64bf42d85d1fdab0da3d6c0bca9dde459b84
m4n1ac47 <m4n1ac47@gmail.com>
2026-05-01 12:40:35 +0300

    Добавил Prettier в проект

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,26 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      prettier: prettierPlugin
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig
     ],
+    rules: {
+      'prettier/prettier': ['error']
+    },
     languageOptions: {
       globals: globals.browser,
     },
